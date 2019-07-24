a46af9cf8c3da2f583265d73423bc80a25ba934b
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-24 11:31:32 -0700

    still working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    
+  
   end
 
   def index
@@ -9,8 +11,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+   
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes:[:name,:quatity])
+
   end
 end
