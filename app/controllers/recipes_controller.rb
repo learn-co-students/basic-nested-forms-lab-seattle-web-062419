af99d416d95a1d1e7ae9fd518ad0adca3c937dea
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-24 11:50:36 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,6 +11,9 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
+    
    
   end
 
@@ -22,7 +25,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, ingredients_attributes:[:name,:quatity])
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name,:quantity])
 
   end
 end
